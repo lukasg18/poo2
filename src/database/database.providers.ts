d75680d658d2b2eb74adb4c710631477ab469a60
lukasg18 <lukas.gomes2010@gmail.com>
2018-09-11 22:35:16 -0300

    conexao com banco

---
+++ src/database/database.providers.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DbConnectionToken',
+    useFactory: async () => await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'postgres',
+      entities: [
+          __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+      synchronize: true,
+    }),
+  },
+];
